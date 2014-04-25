c58e153c56679a1a1327fbf620e52b3ab07ac7af
mpmattoni <matt.mattoni@daosoft.com>
2014-04-24 20:49:41 -0600

    Initial blank JumpJumpSK project.

---
+++ JumpJumpSK/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JumpJumpSK
+//
+//  Created by Matthew Mattoni on 4/24/14.
+//  Copyright (c) 2014 DaoSoft, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JumpJumpSK/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  JumpJumpSK
+//
+
+//  Copyright (c) 2014 DaoSoft, LLC. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
