fceaa665b6fa44822df0184ce8104c58595070c2
mpmattoni <matt.mattoni@daosoft.com>
2014-05-12 21:50:31 -0600

    Added original (uncommented) code for fly paths to touch event.

--- JumpJumpSK/MyScene.m
+++ JumpJumpSK/MyScene.m
@@ -46,17 +46,151 @@
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
     [self.myFrog jump];
+    
     SKShapeNode *yourline = [SKShapeNode node];
-    CGMutablePathRef pathToDraw = CGPathCreateMutable();
-    NSLog(@"Drawing a path from origin: %@ to frame size: %@", NSStringFromCGPoint(self.frame.origin), NSStringFromCGSize(self.frame.size));
-    CGPathMoveToPoint(pathToDraw, NULL, self.frame.origin.x, self.frame.origin.y);
-    CGPathAddLineToPoint(pathToDraw, NULL, self.frame.size.width, self.frame.size.height);
-    yourline.path = pathToDraw;
+    //CGMutablePathRef pathToDraw = CGPathCreateMutable();
+    //NSLog(@"Drawing a path from origin: %@ to frame size: %@", NSStringFromCGPoint(self.frame.origin), NSStringFromCGSize(self.frame.size));
+    //CGPathMoveToPoint(pathToDraw, NULL, self.frame.origin.x, self.frame.origin.y);
+    //CGPathAddLineToPoint(pathToDraw, NULL, self.frame.size.width, self.frame.size.height);
+    yourline.path = [self drawFlyPath:1 setFlightPath:self.frame.size.width];
     [yourline setStrokeColor:[UIColor redColor]];
     [self addChild:yourline];
 }
 
 
+-(CGMutablePathRef)drawFlyPath:(int)level setFlightPath:(int)screenWidth {
+    
+    
+    int screenPosition = (int)screenWidth;
+    srandom(time(NULL));
+    //int direction = (random()%(int)level);
+    UIBezierPath *trackPath = [UIBezierPath bezierPath];
+    [trackPath moveToPoint:CGPointMake(0,200 + 10)];
+    
+    [trackPath addCurveToPoint:CGPointMake(screenPosition, 200 - 5 )
+                 controlPoint1:CGPointMake(600, 400 )
+                 controlPoint2:CGPointMake(800, 300 )];
+    
+    CGPathRef cgPath = trackPath.CGPath;
+    
+    CGMutablePathRef  pathToDraw = CGPathCreateMutableCopy(cgPath);
+
+    return pathToDraw;
+
+}
+
+//-(void) drawFlies:(int *)level setFlightPath:(int *)screenWidth {
+//    
+//    int screenPosition = (int)screenWidth;
+//    srandom( time( NULL ) );
+//    
+//    flies = [[NSMutableArray alloc] init];
+//    for (int i = 0; i < 10; i++) {
+//        
+//        
+//        
+//        //  int displacement = random()%100;
+//        int direction = (random() % (int)level);
+//        
+//        
+//        if (direction == 0) {
+//            UIImage *flyImage = [UIImage imageNamed:@"fly.png"];
+//            fly = [CALayer layer];
+//            fly.contents = (id)flyImage.CGImage;
+//            fly.bounds = CGRectMake(0, 0, flyImage.size.width, flyImage.size.height);
+//            fly.position = CGPointMake(0 + i*20, 300 - i*20);
+//            
+//            [fly setOpacity:1];
+//            CABasicAnimation *buzz = [CABasicAnimation animationWithKeyPath:@"opacity"];
+//            buzz.fromValue = [NSNumber numberWithFloat:0.5];
+//            buzz.toValue = [NSNumber numberWithFloat:1.0];
+//            buzz.duration = 0.1;
+//            buzz.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+//            buzz.repeatCount = HUGE_VALF;
+//            
+//            
+//            UIBezierPath *trackPath = [UIBezierPath bezierPath];
+//            [trackPath moveToPoint:P(0,200 + i*10)];
+//            
+//            [trackPath addCurveToPoint:P(screenPosition, 200 - i*5 )
+//                         controlPoint1:P(600, 400 )
+//                         controlPoint2:P(800, 300 )];
+//            
+//            
+//            
+//            racetrack = [CAShapeLayer layer];
+//            racetrack.path = trackPath.CGPath;
+//            racetrack.strokeColor = [UIColor blackColor].CGColor;
+//            racetrack.fillColor = [UIColor clearColor].CGColor;
+//            racetrack.lineWidth = 2.0;
+//            
+//            
+//            CAKeyframeAnimation *anim = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+//            anim.path = trackPath.CGPath;
+//            anim.rotationMode = kCAAnimationRotateAuto;
+//            anim.repeatCount = HUGE_VALF;
+//            anim.duration = 10 - (int)level + random()%10;
+//            [fly addAnimation:anim forKey:@"position"];
+//            
+//            
+//            //    CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"position"];
+//            //   CGPoint startPt = CGPointMake(fly.bounds.size.width / -2,
+//            //                          fly.position.y );
+//            //  CGPoint endPt = CGPointMake(self.bounds.size.width + flyImage.size.width / 2,
+//            //                      fly.position.y);
+//            // anim.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
+//            // anim.fromValue = [NSValue valueWithCGPoint:endPt];
+//            // anim.toValue = [NSValue valueWithCGPoint:startPt];
+//            // anim.repeatCount = HUGE_VALF;
+//            // anim.duration = 8 + random()%10;
+//            // anim.beginTime = CACurrentMediaTime();
+//            // [fly addAnimation:anim forKey:@"position"];
+//            [fly addAnimation:buzz forKey:@"opacity"];
+//            
+//            [flies addObject:fly];
+//            
+//        } else {
+//            UIImage *flyImage = [UIImage imageNamed:@"fly.png"];
+//            fly = [CALayer layer];
+//            fly.contents = (id)flyImage.CGImage;
+//            fly.bounds = CGRectMake(0, 0, flyImage.size.width, flyImage.size.height);
+//            fly.position = CGPointMake(0,
+//                                       flyImage.size.height / 2 + random()%50 + random()%50 + 200);
+//            
+//            CABasicAnimation *buzz = [CABasicAnimation animationWithKeyPath:@"opacity"];
+//            buzz.fromValue = [NSNumber numberWithFloat:0.5];
+//            buzz.toValue = [NSNumber numberWithFloat:1.0];
+//            buzz.duration = 0.1;
+//            buzz.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+//            buzz.repeatCount = HUGE_VALF;
+//            
+//            
+//            
+//            UIBezierPath *trackPath = [UIBezierPath bezierPath];
+//            [trackPath moveToPoint:P(screenPosition, 300 + random()%20)];
+//            [trackPath addCurveToPoint:P(0, 300 + random()%20)
+//                         controlPoint1:P(150 + random()%100, 100 + random()%20)
+//                         controlPoint2:P(150 + random()%100, 200 + random()%200)];
+//            
+//            CAKeyframeAnimation *anim = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+//            anim.path = trackPath.CGPath;
+//            anim.rotationMode = kCAAnimationRotateAuto;
+//            anim.repeatCount = HUGE_VALF;
+//            anim.duration = 11 - (int)level + random()%10;
+//            [fly addAnimation:anim forKey:@"position"];
+//            
+//            
+//            
+//            [fly addAnimation:buzz forKey:@"opacity"];
+//            
+//            [flies addObject:fly];
+//            
+//        }
+//        
+//        
+//    }
+//    
+//}
 
 
 
